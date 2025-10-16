eafb69bebfbc61f8bb367880d090127069d8b258
abhishek1aids24-star <abhishek1.aids24@cmrit.ac.in>
2025-10-16 10:46:32 +0530

    to calculate sum of two number

---
+++ add.java
@@ -0,0 +1,13 @@
+public class AdditionFixedValues {
+    public static void main(String[] args) {
+       
+        int number1 = 15;
+        int number2 = 20;
+
+        
+        int sum = number1 + number2;
+
+       
+        System.out.println("The sum of " + number1 + " and " + number2 + " is: " + sum);
+    }
+}
